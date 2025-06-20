1a55e03ccf7838da480437cd92fa899084fa74d2
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-06-20 14:10:44 -0300

    feat: Added log writing with file creation and append.

--- python-poo/aula22/log.py
+++ python-poo/aula22/log.py
@@ -1,4 +1,9 @@
 # Abstração
+
+from pathlib import Path
+
+LOG_FILE = Path(__file__) .parent / 'log.txt'
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o metodo log')
@@ -7,7 +12,12 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_fomatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando...')
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_fomatada)
+            arquivo.write('\n')
+
 
 class LogPrintMixin(Log):
     def _log(self, msg):
@@ -15,5 +25,10 @@ class LogPrintMixin(Log):
 
 
 if __name__== '__main__':
-    l = LogPrintMixin()
-    l.log_error('qualquer coisa')
+    lp = LogPrintMixin()
+    lp._log('Teste')
+    lp.log_error('qualquer coisa')
+
+    lf = LogFileMixin() 
+    lf.log_error('qualquer coisa')  
+
